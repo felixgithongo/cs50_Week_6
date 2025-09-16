5e54ff87b46049b5421b5770ecf7eca831695660
felixgithongo <152910666+felixgithongo@users.noreply.github.com>
2025-09-16 12:38:40 +0930

    mario-more.py

---
+++ mario-more.py
@@ -0,0 +1,13 @@
+from cs50 import get_int
+
+
+while True:
+    n = get_int("Height: ")
+    if(1 <= n <= 8):
+        break
+
+for i in range(1, n + 1):
+    print((" " * (n - i)), end="")
+    print(("#" * i), end="")
+    print(("  "), end="")
+    print(("#") * i)
